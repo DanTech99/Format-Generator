59183bf86d462cbde074ec81d3fd8efe4a5152cb
DandevMS <xxdndkxx@gmail.com>
2023-03-09 00:56:14 -0500

    add new funtions, one save data, two send data, add buttons for execute funtions, change forms, renderize data in the table at function .map

--- src/components/FormDatePdf.jsx
+++ src/components/FormDatePdf.jsx
@@ -3,38 +3,58 @@ import { useState, useEffect } from "react"
 
 export default function FormDatePdf() {
 
-      // mantener el input de date actualizado con la fecha actual
-      const [fechaActual, setFechaActual] = useState('');
-      useEffect(() => {
-        const fecha = new Date().toISOString().substr(0, 10);
-        setFechaActual(fecha)
-      }, [])
+       //mantener el input de date actualizado con la fecha actual
+      //  const [fechaActual, setFechaActual] = useState('');
+      //  useEffect(() => {
+      //    const fecha = new Date().toISOString().substr(0, 10);
+      //    setFechaActual(fecha)
+      //  }, [])
+    const [datos, setDatos] = useState({
+      fecha: '',
+      tipo: '',
+      firstFlo: '',
+      finishFlo: '',
+      observaciones: '',
+    })
 
+    const [data, setData] = useState([])
 
-    const [date, setDate] = useState('');
-    const [tipo, setTipo] = useState('');
-    const [firstFlo, setFirstFlo] = useState('');
-    const [finishFlo, setFinishFlo] = useState('');
-    const [observaciones, setObservaciones] = useState('');
-
-    const datos = {date, tipo, firstFlo, finishFlo, observaciones};
-
-    function saveData(datos, e) {
+    // funcion para ir guardando los datos del formulario en un array de objetos
+    const handleInputChange = (e) => {
       e.preventDefault()
-      const [listData, setListData] = useState([])
-      setListData([...listData, datos])
-      console.log(datos)
-    };
+
+      const { name, value } = e.target;
+      setDatos({
+        ...datos,
+        [name] : value
+      })
+    }
+
+    const saveData = (e) => {
+      e.preventDefault();
+      setData(data => [...data, datos])
+      setDatos({
+      fecha: '',
+      tipo: '',
+      firstFlo: '',
+      finishFlo: '',
+      observaciones: '',
+      });
+    
+    }
+    useEffect(() => {
+      console.log(data);
+    }, [data]);
+    
     
 
+    // funcion para tomar los datos del formulario y enviarlos al servidor, el cual respondara enviando un pdf con la plantilla y los datos suministrados
     const handleSubmit = async (e) => {
-      e.preventDefault();
-
-  
+      e.preventDefault()
        const response = await fetch('http://localhost:3000/formulario', {
          method: 'POST',
          headers: {'Content-Type': 'application/json'},
-         body: JSON.stringify(datos)
+         body: JSON.stringify(data)
        })
        if (response.ok) {
         // descargar el pdf
@@ -47,56 +67,45 @@ export default function FormDatePdf() {
         link.click();
         document.body.removeChild(link)
        }
-     
-
-      e.target.value = null;
     }
     
   return (
     <>
-    <div class="container-fluid text-center">
-      <div class="row">
-        <div class="col-6">
-          <form  className="container w-75 border p-5" onSubmit={handleSubmit}>
+    <div className="container-fluid text-center">
+      <div className="row">
+        <div className="col-6">
+          <form  className="container w-75 border p-5">
             <div className="mb-3">
               <label htmlFor="fecha" className="form-label">Fecha de ingreso (dd/mmm/aaaa)</label>
-              <input type="date" className="form-control" id="fecha" defaultValue={fechaActual} name="date" onChange={event => setDate(event.target.value)} />
+              <input type="date" className="form-control" id="fecha" value={datos.fecha} name="fecha" onChange={handleInputChange}  />
             </div>
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label" >Tipo documental</label>
-              <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="eje: Solicitud" name="tipo" onChange={event => setTipo(event.target.value)} defaultValue={tipo} />
+              <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="eje: Solicitud" name="tipo" onChange={handleInputChange} value={datos.tipo} />
             </div>
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label">No. de Folio que inicia</label>
-              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 01" name="firstFlo" onChange={event => setFirstFlo(event.target.value)} defaultValue={firstFlo}/>
+              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 01" name="firstFlo" onChange={handleInputChange} value={datos.firstFlo}/>
             </div>
             <div className="mb-3">
               <label htmlFor="exampleFormControlInput1" className="form-label">No. de Folio que finaliza</label>
-              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 02" name="finishFlo" onChange={event => setFinishFlo(event.target.value)} defaultValue={finishFlo}/>
+              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 02" name="finishFlo" onChange={handleInputChange} value={datos.finishFlo}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">Observaciones</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ejem: solicitud cambio de propietario" name="observaciones" onChange={event => setObservaciones(event.target.value)} defaultValue={observaciones}></textarea>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ejem: solicitud cambio de propietario" name="observaciones" onChange={handleInputChange} value={datos.observaciones}></textarea>
             </div>
 
             <div className="mb-3 mt-4 d-flex justify-content-center">
-              <button type="submit" className="btn btn-primary mb-3">Generar pdf</button>
+              <button type="submit" className="btn btn-primary mb-3" onClick={saveData}>guardar</button>
             </div>
-            
-
-            {/* Mostrar los datos almacenados en el estado */}
-          {/* <pre>{JSON.stringify(datos, null, 2)}</pre> */}
           </form>
-          <div className="mb-3 mt-4 d-flex justify-content-center">
-              <button type="submit" className="btn btn-primary mb-3" onChange={saveData}>Guardar</button>
-            </div>
         </div>
-        <div class="col-6">
-        <div class="table-responsive">
+        <div className="col-6">
+        <div className="table-responsive">
         <table className="table">
           <thead>
             <tr>
-            
               <th scope="col">fecha</th>
               <th scope="col">Tipo de proceso</th>
               <th scope="col">numero de folio que inicia</th>
@@ -105,20 +114,22 @@ export default function FormDatePdf() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>{datos.date}</td>
-              <td>{datos.tipo}</td>
-              <td>{datos.firstFlo}</td>
-              <td>{datos.finishFlo}</td>
-              <td>{datos.observaciones}</td>
+            {data.map((dato, index) => (
+            <tr key={index}>
+              <td>{dato.fecha}</td>
+              <td>{dato.tipo}</td>
+              <td>{dato.firstFlo}</td>
+              <td>{dato.finishFlo}</td>
+              <td>{dato.observaciones}</td>
             </tr>
+              ))}
           </tbody>
         </table>
+        <button type="submit" className="btn btn-primary btn-sm" onClick={handleSubmit}>Generar pdf</button>
         </div>
         </div>
       </div>
     </div>
-  
     </>
   )
 }
