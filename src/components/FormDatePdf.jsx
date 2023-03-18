97242db4f9fff9ca5cbff9e548e5787c957280c8
DandevMS <xxdndkxx@gmail.com>
2023-03-18 00:37:43 -0500

    add tailwindscss, remove bootstrap, edit formdat with tailwindscss

--- src/components/FormDatePdf.jsx
+++ src/components/FormDatePdf.jsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 export default function FormDatePdf() {
     const [datos, setDatos] = useState({
@@ -33,7 +33,6 @@ export default function FormDatePdf() {
       });
     
     }
-    console.log(data)
     // funcion para tomar los datos del formulario y enviarlos al servidor, el cual respondara enviando un pdf con la plantilla y los datos suministrados
     const handleSubmit = async (e) => {
       e.preventDefault()
@@ -61,29 +60,29 @@ export default function FormDatePdf() {
     
   return (
     <>
-    <div className="container-fluid text-center">
-      <div className="row">
-        <div className="col-6">
+    <div className="container text-center">
+      <div className="grid md:grid-cols-2 grid-cols-1">
+        <div className="container">
           <form  className="container w-75 border p-5">
-            <div className="mb-3">
+            <div className="mb-3 flex flex-col">
               <label htmlFor="fecha" className="form-label">Fecha de ingreso (dd/mmm/aaaa)</label>
-              <input type="date" className="form-control" id="fecha" value={datos.fecha} name="fecha" onChange={handleInputChange}  />
+              <input type="date" className="form-input px-4 py-3 rounded-lg" id="fecha" value={datos.fecha} name="fecha" onChange={handleInputChange}  />
             </div>
-            <div className="mb-3">
+            <div className="mb-3 flex flex-col">
               <label htmlFor="exampleFormControlInput1" className="form-label" >Tipo documental</label>
-              <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="eje: Solicitud" name="tipo" onChange={handleInputChange} value={datos.tipo} />
+              <input type="text" className="form-input px-4 py-3 rounded-lg" id="exampleFormControlInput1" placeholder="eje: Solicitud" name="tipo" onChange={handleInputChange} value={datos.tipo} />
             </div>
-            <div className="mb-3">
+            <div className="mb-3 flex flex-col">
               <label htmlFor="exampleFormControlInput1" className="form-label">No. de Folio que inicia</label>
-              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 01" name="firstFlo" onChange={handleInputChange} value={datos.firstFlo}/>
+              <input type="number" className="form-input px-4 py-3 rounded-lg" id="exampleFormControlInput1" placeholder="eje: 01" name="firstFlo" onChange={handleInputChange} value={datos.firstFlo}/>
             </div>
-            <div className="mb-3">
+            <div className="mb-3 flex flex-col">
               <label htmlFor="exampleFormControlInput1" className="form-label">No. de Folio que finaliza</label>
-              <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="eje: 02" name="finishFlo" onChange={handleInputChange} value={datos.finishFlo}/>
+              <input type="number" className="form-input px-4 py-3 rounded-lg" id="exampleFormControlInput1" placeholder="eje: 02" name="finishFlo" onChange={handleInputChange} value={datos.finishFlo}/>
             </div>
-            <div className="mb-3">
+            <div className="mb-3 flex flex-col">
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">Observaciones</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="ejem: solicitud cambio de propietario" name="observaciones" onChange={handleInputChange} value={datos.observaciones}></textarea>
+                <textarea className="form-input px-4 py-3 rounded-lg" id="exampleFormControlTextarea1" rows="3" placeholder="ejem: solicitud cambio de propietario" name="observaciones" onChange={handleInputChange} value={datos.observaciones}></textarea>
             </div>
 
             <div className="mb-3 mt-4 d-flex justify-content-center">
@@ -91,38 +90,36 @@ export default function FormDatePdf() {
             </div>
           </form>
         </div>
-        <div className="col-6">
-        <div className="table-responsive">
-        <table className="table">
-          <thead>
-            <tr>
-            <th scope="col">item</th>
-              <th scope="col">fecha</th>
-              <th scope="col">Tipo de proceso</th>
-              <th scope="col">numero de folio que inicia</th>
-              <th scope="col">numero de folio que finaliza</th>
-              <th scope="col">observaciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((dato, index) => (
-            <tr key={index}>
-              <td>{index}</td>
-              <td>{dato.fecha}</td>
-              <td>{dato.tipo}</td>
-              <td>{dato.firstFlo}</td>  
-              <td>{dato.finishFlo}</td>
-              <td>{dato.observaciones}</td>
-              <button type="button" className="btn bg-danger btn-sm " onClick={() => deleteItem(index)}>Eliminar</button>
-            </tr>
-              ))}
-          </tbody>
-        </table>
-        <button type="submit" className="btn btn-primary btn-sm" onClick={handleSubmit}>Generar pdf</button>
+        <div className="container p-3">
+          <table className="md:table-fixed  border-collapse border border-slate-500">
+            <thead>
+              <tr className="bg-blue-500 text-white">
+                <th className="border border-slate-600 p-2">item</th>
+                <th className="border border-slate-600 p-2">fecha</th>
+                <th className="border border-slate-600 p-2">Tipo de proceso</th>
+                <th className="border border-slate-600 p-2">numero de folio que inicia</th>
+                <th className="border border-slate-600 p-2">numero de folio que finaliza</th>
+                <th className="border border-slate-600 p-2">observaciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((dato, index) => (
+              <tr key={index}>
+                <td className="border border-slate-700 p-2">{index}</td>
+                <td className="border border-slate-700 p-2">{dato.fecha}</td>
+                <td className="border border-slate-700 p-2">{dato.tipo}</td>
+                <td className="border border-slate-700 p-2">{dato.firstFlo}</td>  
+                <td className="border border-slate-700 p-2">{dato.finishFlo}</td>
+                <td className="border border-slate-700 p-2">{dato.observaciones}</td>
+                {/* <button type="button" className="btn bg-danger btn-sm " onClick={() => deleteItem(index)}>Eliminar</button> */}
+              </tr>
+                ))}
+            </tbody>
+          </table>
+          <button type="submit" className="btn btn-primary btn-sm" onClick={handleSubmit}>Generar pdf</button>
         </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,15 +1,14 @@
-
+import PreselectionDemo from "./SwitchBtnMode"
 
 function HeaderNav() {
   return (
-    <header className="container-fluid  bg-success ">
-      <nav className=" container-xl d-flex p-2 justify-content-between align-items-center ">
-        <div className="title d-flex justify-content-start align-items-center text-light">
-          <a href="" className="text-light fs-2 text-decoration-none">Gestion Documental | Generador de hojas de control</a>
+    <header className="container-fluid mx-auto  bg-blue-500 p-2 ">
+      <nav className=" container m-auto d-flex p-2 flex justify-between align-items-center ">
+        <div className="text-white text-2xl">
+          <a href="" className="">Gestion Documental | Generador de hojas de control</a>
         </div>
-        <div className=" mode d-flex justify-content-end align-items-center">
-            <a href="" className="me-1 btn bg-light">dark</a>
-            <a href="" className="me-1 btn bg-light">light</a>
+        <div className="text-white items-center content-center">
+            <PreselectionDemo/>
         </div>
       </nav>
         
