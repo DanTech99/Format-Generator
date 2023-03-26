4612971ee21becdbacdfcf42b49696511285a723
DandevMs <xxdndkxx@gmail.com>
2023-03-26 15:47:14 -0500

    change url server

--- src/components/formGenerate/GeneratePdf.jsx
+++ src/components/formGenerate/GeneratePdf.jsx
@@ -203,7 +203,7 @@ export default function GeneratePdf() {
    */
   const handleSubmit = async (e) => {
     e.preventDefault()
-     const response = await fetch('http://localhost:3001/generatecontrolpdf', {
+     const response = await fetch('https://server-generate.up.railway.app/generatecontrolpdf', {
        method: 'POST',
        headers: {'Content-Type': 'application/json'},
        body: JSON.stringify(data)
